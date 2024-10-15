c58e2d0962482ba2bb90e5083b35bf796e232ebc
Ganeshswaminathan R <ganeshswaminathanr@gmail.com>
2024-10-15 09:59:52 +0530

    Added rotate

--- src/Array/main.py
+++ src/Array/main.py
@@ -9,6 +9,19 @@ def shiftLeft(arr: list, n=1) -> list:
         idx += 1
 
 
+def rotateLeft(arr: list, n=1) -> list:
+    idx = 0
+    temp = []
+    while idx < len(arr):
+        temp.append(arr[idx])
+        if n >= len(arr):
+            arr[idx] = temp.pop(0)
+        else:
+            arr[idx] = arr[n]
+            n += 1
+        idx += 1
+
+
 def shiftRight(arr: list, n=1):
     idx = len(arr) - 1
     n = len(arr) - n - 1
@@ -21,9 +34,23 @@ def shiftRight(arr: list, n=1):
         idx -= 1
 
 
+def rotateRight(arr: list, n=1):
+    idx = len(arr) - 1
+    n = len(arr) - n - 1
+    temp = []
+    while idx >= 0:
+        temp.append(arr[idx])
+        if n < 0:
+            arr[idx] = temp.pop(0)
+        else:
+            arr[idx] = arr[n]
+            n -= 1
+        idx -= 1
+
+
 def main():
     l = [1, 2, 3, 4, 5]
-    shiftRight(l, 10)
+    rotateRight(l, 3)
     print(l)
 
 
